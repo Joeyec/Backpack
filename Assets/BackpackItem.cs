fdad9467d24d644e2ebcae1957cd86176f0d145e
Joeyec <1329097376@qq.com>
2019-09-19 10:20:11 +0800

    push Backpack

---
+++ Assets/BackpackItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BackpackItem 
+{
+    public enum ItemType
+    {
+        UnKnown = -1,
+        Equip,
+        Chips,
+    }
+    public int ItemID;
+    public ItemType itemType = ItemType.UnKnown;
+    public string ItemName;
+    public string ItemDesc;
+    public string ItemIcon;
+    public string ItemBgIcon;
+    public int ItemCount;
+    public int ItemQua;
+
+    public int ItemOperation;
+    public BackpackItem(int itemid,string name,string desc,int itemtype,string icon,string bgicon,int count,int quality,int operation)
+    {
+        ItemID = itemid;
+        ItemName = name;
+        ItemDesc = desc;
+        switch (itemtype)
+        {
+            case -1:
+                itemType = ItemType.UnKnown;
+                break;
+            case 0:
+                itemType = ItemType.Equip;
+                break;
+            case 1:
+                itemType = ItemType.Chips;
+                break;
+        }
+        ItemIcon = icon;
+        ItemBgIcon = bgicon;
+        ItemCount = count;
+        ItemQua = quality;
+        ItemOperation = operation;
+
+    }
+}
---
+++ Assets/NewItemManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+using System.Text;
+
+public class NewItemManager
+{
+    private static NewItemManager instance;
+    public static NewItemManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = new NewItemManager();
+            return instance;
+        }
+    }
+    public List<BackpackItem> BackpackItemList;
+    private JsonData ItemConfig;
+    public void LoadItemConfigData()
+    {
+        if (BackpackItemList == null)
+        {
+            BackpackItemList = new List<BackpackItem>();
+        }
+        this.ItemConfig=JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Config/BackpackItem.json"));
+        //, Encoding.GetEncoding("GB2312")
+
+    }
+    private void DecodeJson()
+    {
+        for (int i = 0; i < this.ItemConfig.Count; i++)
+        {
+            int itemID = (int)this.ItemConfig[i]["ItemID"];
+            string itemName = ItemConfig[i]["ItemName"].ToString();
+            string itemDesc = ItemConfig[i]["ItemDesc"].ToString();
+            int itemType = (int)ItemConfig[i]["ItemType"];
+            string itemIcon = ItemConfig[i]["ItemIcon"].ToString();
+            string itemBgIcon = ItemConfig[i]["ItemBgIcon"].ToString();
+            int itemCount = (int)ItemConfig[i]["ItemCount"];
+            int itemQuality = (int)ItemConfig[i]["ItemQuality"];
+            int itemOperation = (int)ItemConfig[i]["ItemOperation"];
+            Debug.Log(itemID + " " + itemName + " " + itemDesc + " " + itemType + " " + itemIcon + " " + itemBgIcon + " " +
+                itemCount + " " + itemQuality + " " + itemOperation);
+            BackpackItem item = new BackpackItem(itemID, itemName, itemDesc, itemType, itemIcon, itemBgIcon, itemCount, itemQuality, itemOperation);
+            BackpackItemList.Add(item);
+
+        }
+    }
+}
+ 
